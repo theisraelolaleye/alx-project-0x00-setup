632b1e0706093c960a342ffe02a235f6003ce034
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-16 12:25:55 +0100

    Fix Pill component and update interfaces/index.ts for type safety

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,7 @@
+import { PillProps } from "@/interfaces";
 
 
-
-const Pill: React.FC = ({ title }: { title: string }) => {
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
       <p className=" text-sm ">Title</p>
---
+++ alx-project-0x00/interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface PillProps {
+  title: string;
+}
+
+export interface CardProps {
+  imageSrc: string;
+  title: string;
+  location: string;
+  rating: number;
+  pricePerNight: number;
+  features: string[];
+}
